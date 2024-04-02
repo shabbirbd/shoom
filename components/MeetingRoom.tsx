11cbed74e4685b4dcc50dd25dfc0ad7a8b18f517
shabbirbd <shabbirhossainshah@gmail.com>
2024-04-02 20:49:33 +0600

    eeting setup done

---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,9 @@
+const MeetingRoom = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default MeetingRoom;
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -68,7 +68,6 @@ const MeetingTypeList = () => {
     }
   };
 
-  console.log(meetingState);
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
